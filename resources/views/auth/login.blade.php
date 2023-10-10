f1d6268740b2d005b3fed1bda28b8fb456eff830
Le Ha Phuong <minialpha1234567@gmail.com>
2023-10-10 23:48:36 +0700

    Sửa file đăng nhập đăng ký

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,17 +4,17 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-4">
-            <h2 class="mb-4">{{ __('Đăng nhập') }}</h2>
+            <h2 class="mb-4 text-center">{{ __('Đăng nhập') }}</h2>
             
             <form method="POST" action="{{ route('login') }}">
                 @csrf
 
                 <div class="mb-3">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus
-                        placeholder="Nhập chỉ email">
+                    <input id="username" type="username" class="form-control bg-white @error('username') is-invalid @enderror"
+                        name="username" value="{{ old('username') }}" required autocomplete="username" autofocus
+                        placeholder="Tên đăng nhập">
 
-                    @error('email')
+                    @error('username')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
@@ -22,8 +22,8 @@
                 </div>
 
                 <div class="mb-3">
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-                        name="password" required autocomplete="current-password" placeholder="Nhập mật khẩu">
+                    <input id="password" type="password" class="form-control bg-white @error('password') is-invalid @enderror"
+                        name="password" required autocomplete="current-password" placeholder="Mật khẩu">
 
                     @error('password')
                     <span class="invalid-feedback" role="alert">
@@ -34,21 +34,27 @@
 
                 <div class="row">
                     <div class="col-md-12">
-                        <button type="submit" class="btn btn-primary">
+                        <button type="submit" class="btn btn-primary w-100">
                             {{ __('Đăng nhập') }}
                         </button>
 
-                        @if (Route::has('password.request'))
-                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                            {{ __('Quên mật khẩu?') }}
-                        </a>
-                        @endif
                     </div>
                 </div>
-            </form>
-        </div>
-        <div class="col-md-8">
+            </form>              
             
+            <div class="row">
+                @if (Route::has('password.request'))
+                <a class="btn btn-link" href="{{ route('password.request') }}">
+                    {{ __('Quên mật khẩu?') }}
+                </a>
+                @endif
+            </div>
+
+            <div>
+                <a type="button" class="btn btn-secondary w-100 mt-3 bordered" href="/register">
+                    Đăng ký
+                </a>
+            </div>
         </div>
     </div>
 </div>
